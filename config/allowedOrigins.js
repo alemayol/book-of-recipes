f894efd7ba02d5d372572311db435f918413fae5
Alejandro Mayol <alejandromayol.dev@gmail.com>
2023-08-09 19:40:07 -0400

    Added new allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://frolicking-centaur-3c1f61.netlify.app/"];
+const allowedOrigins = [
+  "https://frolicking-centaur-3c1f61.netlify.app",
+  "https://main--frolicking-centaur-3c1f61.netlify.app",
+];
 
 export default allowedOrigins;
